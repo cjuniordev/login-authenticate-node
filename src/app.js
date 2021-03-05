1212db0e5af2693cff6a4f3730cd24d377ab3c9c
cjuniordev <carlosrogeriocr02@gmail.com>
2021-03-05 11:40:09 -0300

    config cors

--- src/app.js
+++ src/app.js
@@ -23,12 +23,7 @@ const authRoutes = require('./routes/auth.routes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.json({ type: 'application/vnd.api+json' }));
-app.use(
-  cors({
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200,
-  })
-);
+app.use(cors('*'));
 
 app.use(index);
 app.use(userRoutes);
